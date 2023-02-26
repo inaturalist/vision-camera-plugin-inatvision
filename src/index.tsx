c4abbe42cc3211469511c75562a7d7cc11860392
Johannes Klein <johannes.t.klein@gmail.com>
2023-02-26 21:26:20 +0100

    Update type

--- src/index.tsx
+++ src/index.tsx
@@ -13,14 +13,10 @@ interface Prediction {
   [rank: string]: PredictionDetails[];
 }
 
-interface ImageLabel {
-  predictions: Prediction[];
-}
-
 /**
  * Returns an array of matching `ImageLabel`s for the given frame. *
  */
-export function inatVision( frame: Frame, modelPath: string, taxonomyPath: string ): ImageLabel[] {
+export function inatVision( frame: Frame, modelPath: string, taxonomyPath: string ): Prediction[] {
   "worklet";
   // @ts-expect-error Frame Processors are not typed.
   return __inatVision( frame, modelPath, taxonomyPath );

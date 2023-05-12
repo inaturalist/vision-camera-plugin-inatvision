c07812d61a90e46ebbd834ba3b68e1cd6bd9a940
Johannes Klein <johannes.t.klein@gmail.com>
2023-05-12 13:00:43 +0200

    feat: add JS side functions o expose listener

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 /* globals __inatVision */
 import type { Frame } from 'react-native-vision-camera';
+import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
+import type { EmitterSubscription } from 'react-native';
 
 interface PredictionDetails {
   ancestor_ids: number[];
@@ -35,4 +37,53 @@ export function inatVision(
     filterByTaxonId,
     negativeFilter
   );
+}
+
+const LINKING_ERROR =
+  `The package 'vision-camera-plugin-inatvision' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' });
+
+const VisionCameraPluginInatVision = NativeModules.VisionCameraPluginInatVision
+  ? NativeModules.VisionCameraPluginInatVision
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+interface State {
+  eventListener: null | EmitterSubscription;
+}
+
+const state: State = {
+  eventListener: null,
+};
+
+/**
+ *  Adds a listener for the camera log event
+ */
+export function addListener(callback: Function): void {
+  // Remove the previous listener if it exists
+  if (state.eventListener) {
+    state.eventListener.remove();
+  }
+  // Register the listener
+  const eventEmitter = new NativeEventEmitter(VisionCameraPluginInatVision);
+  state.eventListener = eventEmitter.addListener('CameraLog', (event) => {
+    callback(event);
+  });
+}
+
+/**
+ *  Removes the listener
+ */
+export function removeListener(): void {
+  // Remove the listener if it exists
+  if (!state.eventListener) {
+    return;
+  }
+  state.eventListener.remove();
 }

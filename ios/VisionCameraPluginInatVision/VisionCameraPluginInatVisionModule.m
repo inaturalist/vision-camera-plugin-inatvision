a448a7ffc3c0e13eddcb4c5b68c512c9f12cff47
Johannes Klein <johannes.t.klein@gmail.com>
2023-12-03 14:39:34 +0100

    chore: remove unused new arch code

--- ios/VisionCameraPluginInatVision/VisionCameraPluginInatVisionModule.m
+++ ios/VisionCameraPluginInatVision/VisionCameraPluginInatVisionModule.m
@@ -5,15 +5,9 @@
 #import "VCPTaxonomy.h"
 #import "VCPPrediction.h"
 
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNAwesomeModuleSpec.h"
-
-@interface AwesomeModule : NSObject <NativeAwesomeModuleSpec>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface AwesomeModule : NSObject <RCTBridgeModule>
-#endif
 @end
 
 @implementation AwesomeModule

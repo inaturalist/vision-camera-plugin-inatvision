d49b8d933c640a84a7cbd1003b7dc014b315fe90
Johannes Klein <johannes.t.klein@gmail.com>
2024-02-28 13:58:03 +0100

    fix: need to call worklet from another worklet

--- src/index.tsx
+++ src/index.tsx
@@ -20,6 +20,7 @@ export interface Prediction {
 const supportedVersions = ['1.0', '2.3', '2.4' as const];
 
 function optionsAreValid(options: Options | OptionsForImage) {
+  'worklet';
   if (!supportedVersions.includes(options.version)) {
     throw new Error('This model version is not supported.');
   }

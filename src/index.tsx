a8f9ec4013799db01e1c95f9ae75d2b0bd6877f2
Johannes Klein <johannes.t.klein@gmail.com>
2024-02-29 17:35:07 +0100

    feat: add rank string if not given from native side

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,60 @@ import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
 import type { EmitterSubscription } from 'react-native';
 import type { Frame } from 'react-native-vision-camera';
 
+const LINKING_ERROR =
+  `The package 'vision-camera-plugin-inatvision' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' });
+
+const VisionCameraPluginInatVision = NativeModules.VisionCameraPluginInatVision
+  ? NativeModules.VisionCameraPluginInatVision
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+class INatVisionError extends Error {}
+Object.defineProperty(INatVisionError.prototype, 'name', {
+  value: 'INatVisionError',
+});
+
+interface State {
+  eventListener: null | EmitterSubscription;
+}
+
+const state: State = {
+  eventListener: null,
+};
+
+/**
+ *  Adds a listener for the camera log event
+ */
+export function addLogListener(callback: Function): void {
+  // Remove the previous listener if it exists
+  if (state.eventListener) {
+    state.eventListener.remove();
+  }
+  // Register the listener
+  const eventEmitter = new NativeEventEmitter(VisionCameraPluginInatVision);
+  state.eventListener = eventEmitter.addListener('CameraLog', (event) => {
+    callback(event);
+  });
+}
+
+/**
+ *  Removes the listener
+ */
+export function removeLogListener(): void {
+  // Remove the listener if it exists
+  if (!state.eventListener) {
+    return;
+  }
+  state.eventListener.remove();
+}
+
 enum RANK {
   'stateofmatter' = 'stateofmatter',
   'kingdom' = 'kingdom',
@@ -138,6 +192,20 @@ function optionsAreValid(options: Options | OptionsForImage): boolean {
   return true;
 }
 
+function handleResult(result: any): Result {
+  'worklet';
+  const predictions = result.predictions.map((prediction: Prediction) => ({
+    ...prediction,
+    rank: prediction.rank
+      ? prediction.rank
+      : mapLevelToRank[prediction.rank_level],
+  }));
+  return {
+    ...result,
+    predictions,
+  };
+}
+
 interface Options {
   // Required
   version: string;
@@ -156,61 +224,9 @@ export function inatVision(frame: Frame, options: Options): Result {
   'worklet';
   optionsAreValid(options);
   // @ts-expect-error Frame Processors are not typed.
-  return __inatVision(frame, options);
-}
-
-const LINKING_ERROR =
-  `The package 'vision-camera-plugin-inatvision' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' });
-
-const VisionCameraPluginInatVision = NativeModules.VisionCameraPluginInatVision
-  ? NativeModules.VisionCameraPluginInatVision
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
-
-interface State {
-  eventListener: null | EmitterSubscription;
-}
-
-class INatVisionError extends Error {}
-Object.defineProperty(INatVisionError.prototype, 'name', {
-  value: 'INatVisionError',
-});
-
-const state: State = {
-  eventListener: null,
-};
-
-/**
- *  Adds a listener for the camera log event
- */
-export function addLogListener(callback: Function): void {
-  // Remove the previous listener if it exists
-  if (state.eventListener) {
-    state.eventListener.remove();
-  }
-  // Register the listener
-  const eventEmitter = new NativeEventEmitter(VisionCameraPluginInatVision);
-  state.eventListener = eventEmitter.addListener('CameraLog', (event) => {
-    callback(event);
-  });
-}
-
-/**
- *  Removes the listener
- */
-export function removeLogListener(): void {
-  // Remove the listener if it exists
-  if (!state.eventListener) {
-    return;
-  }
-  state.eventListener.remove();
+  const result = __inatVision(frame, options);
+  const handledResult = handleResult(result);
+  return handledResult;
 }
 
 interface OptionsForImage {

9bbc7b06d3955bc18e550aaf5055fc60974b907d
Johannes Klein <johannes.t.klein@gmail.com>
2024-03-01 15:33:28 +0100

    chore: add doc comments

--- src/index.tsx
+++ src/index.tsx
@@ -236,20 +236,50 @@ function handleResult(result: any, options: Options): Result {
   return handledResult;
 }
 
+/**
+ * Represents the options for a call to use the plugin to predict on a frame.
+ */
 interface Options {
   // Required
+  /**
+   * The version of the model to use.
+   */
   version: string;
+  /**
+   * The path to the model file.
+   */
   modelPath: string;
+  /**
+   * The path to the taxonomy file.
+   */
   taxonomyPath: string;
   // Optional
+  /**
+   * The confidence threshold for the predictions.
+   */
   confidenceThreshold?: string;
+  /**
+   * The iconic taxon id to filter by.
+   */
   filterByTaxonId?: null | string;
+  /**
+   * The spatial taxon id to filter by.
+   */
   negativeFilter?: null | boolean;
+  /**
+   * The number of results to keep stored internally.
+   *
+   * Specifies the integer number of results to store internally that the plugin serves the best out of.
+   * E.g. if the plugin is called with this number set to 5, the plugin will serve the best result out of the 5 stored previous results.
+   * Setting this number to 0 or 1 will always return the current result (i.e. none or only one frame result will be stored at a time).
+   */
   numStoredResults?: number;
 }
 
 /**
- * Returns an array of matching `ImageLabel`s for the given frame. *
+ * Function to call the computer vision model with a frame from the camera
+ * @param frame The frame to predict on.
+ * @param options The options for the prediction.
  */
 export function inatVision(frame: Frame, options: Options): Result {
   'worklet';

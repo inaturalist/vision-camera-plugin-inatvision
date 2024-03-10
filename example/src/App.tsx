a963b01cdbf60a1513d449b7575512fbc39feda5
Johannes Klein <johannes.t.klein@gmail.com>
2024-03-10 11:48:52 +0100

    fix: remove gpu buffer flag

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -281,10 +281,6 @@ export default function App() {
           enableZoomGesture
           pixelFormat={Platform.OS === 'ios' ? 'native' : 'yuv'}
           resizeMode="contain"
-          // As of vision-camera@3.9.0.beta.4, this seems to be necessary to avoid stuttering on Pixel
-          // On Samsung this might cause some crashes though, so in production we might have to
-          // selectively enable this based on the device model
-          enableGpuBuffers={true}
           enableFpsGraph={true}
         />
         <View style={styles.row}>

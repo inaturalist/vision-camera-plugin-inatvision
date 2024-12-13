d76bc008b4ae1315c6494684cb970972752d472c
Johannes Klein <johannes.t.klein@gmail.com>
2024-12-13 11:32:11 +0100

    Add elevation lookup table

--- src/lookUpLocation.ts
+++ src/lookUpLocation.ts
@@ -19,10 +19,13 @@ export function lookUpLocation(location: Location): LocationLookup {
   // # lookup the H3 cell this lat lng occurs in
   const h3Index = latLngToCell(location.latitude, location.longitude, 4);
   const h3CellCentroid = cellToLatLng(h3Index);
+  // Every h3 cell string starts with "84" which we can remove from lookup table and here
+  // Every h3 cell string ends with a stretch of f's which we can remove from lookup table and here
+  const shortenedH3Index = h3Index.slice(2, 7);
 
   // # get the average elevation of the above H3 cell
   // Read the elevation from the lookup table return a minus elevation if h3Index is not found
-  const elevation = elevationLookupDictTyped[h3Index] || -32768.0;
+  const elevation = elevationLookupDictTyped[shortenedH3Index] || -32768.0;
   const locationLookup = {
     latitude: h3CellCentroid[0],
     longitude: h3CellCentroid[1],

e312bdee738927f5007f41509c231d5399b3a141
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-09-05 22:49:27 +0200

    Converter functions taken from HybridResizer.swift of vc-resizer nitro module

--- ios/HybridVisionCameraPluginInatVision.swift
+++ ios/HybridVisionCameraPluginInatVision.swift
@@ -5,4 +5,22 @@ class HybridVisionCameraPluginInatVision: HybridVisionCameraPluginInatVisionSpec
   func call(frame: any HybridFrameSpec, options: AnyMap) -> AnyMap {
     return options;
   }
+
+  private func sampleBuffer(from frame: any HybridFrameSpec) throws -> CMSampleBuffer {
+    guard let nativeFrame = frame as? any NativeFrame else {
+      throw RuntimeError.error(withMessage: "The given Frame is not of type `NativeFrame`!")
+    }
+    guard let sampleBuffer = nativeFrame.sampleBuffer else {
+      throw RuntimeError.error(withMessage: "The given Frame's `sampleBuffer` is no longer valid!")
+    }
+    return sampleBuffer
+  }
+
+  private func pixelBuffer(from sampleBuffer: CMSampleBuffer) throws -> CVPixelBuffer {
+    guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+      throw RuntimeError.error(
+        withMessage: "The given Frame does not contain a valid image buffer!")
+    }
+    return pixelBuffer
+  }
 }

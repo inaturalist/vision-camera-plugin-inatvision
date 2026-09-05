4ed7514925f137c71602586aa83875b3b4b1c17d
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-09-06 00:12:21 +0200

    Refactor penalty to worklets

--- src/index.tsx
+++ src/index.tsx
@@ -721,31 +721,30 @@ function handleResult(result: any, options: Options): Result {
 
   let current: Result = result;
 
-  // if (maxNumStoredResults > 1) {
-  //   const currentLastPrediction =
-  //     current.predictions[current.predictions.length - 1];
-  //   let currentScore = currentLastPrediction?.score || 0;
+  if (maxNumStoredResults > 1) {
+    const currentLastPrediction =
+      current.predictions[current.predictions.length - 1];
+    let currentScore = currentLastPrediction?.score || 0;
 
-  //   const penaltyIncrement = 0.5 / (maxNumStoredResults - 1);
-  //   // Select the best result from the stored results
-  //   for (let i = state.storedResults.value.length - 1; i >= 0; i--) {
-  //     const candidateResult = state.storedResults.value[i];
-  //     if (!candidateResult) {
-  //       break;
-  //     }
-  //     const candidateLastPrediction =
-  //       candidateResult.predictions[candidateResult.predictions.length - 1];
-  //     const candidateScore = candidateLastPrediction?.score || 0;
+    const penaltyIncrement = 0.5 / (maxNumStoredResults - 1);
+    // Select the best result from the stored results
+    for (let i = storedResults.length - 1; i >= 0; i--) {
+      const candidateResult = storedResults[i];
+      if (!candidateResult) {
+        break;
+      }
+      const candidateLastPrediction =
+        candidateResult.predictions[candidateResult.predictions.length - 1];
+      const candidateScore = candidateLastPrediction?.score || 0;
 
-  //     const penalty =
-  //       1 - penaltyIncrement * (state.storedResults.value.length - 1 - i);
+      const penalty = 1 - penaltyIncrement * (storedResults.length - 1 - i);
 
-  //     if (candidateScore * penalty > currentScore) {
-  //       current = candidateResult;
-  //       currentScore = candidateScore;
-  //     }
-  //   }
-  // }
+      if (candidateScore * penalty > currentScore) {
+        current = candidateResult;
+        currentScore = candidateScore;
+      }
+    }
+  }
 
   const predictions = current.predictions
     // only KPCOFGS ranks qualify as "top" predictions

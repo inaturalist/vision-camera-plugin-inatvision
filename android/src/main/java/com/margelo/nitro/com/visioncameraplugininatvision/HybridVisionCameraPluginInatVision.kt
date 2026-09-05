25c13dafdbdc5b4bc9df4add1fb528eb4d85e436
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-09-06 00:40:40 +0200

    Kotlin file according to module spec

---
+++ android/src/main/java/com/margelo/nitro/com/visioncameraplugininatvision/HybridVisionCameraPluginInatVision.kt
@@ -0,0 +1,7 @@
+package com.margelo.nitro.com.visioncameraplugininatvision
+import com.margelo.nitro.camera.HybridFrameSpec
+import com.margelo.nitro.core.AnyMap
+class HybridVisionCameraPluginInatVision : HybridVisionCameraPluginInatVisionSpec() {
+  override fun call(frame: HybridFrameSpec, options: AnyMap): AnyMap {
+  }
+}

b7e53c9e60de3a6d84be615c68882584636d294f
Johannes Klein <17345891+jtklein@users.noreply.github.com>
2026-09-06 10:59:22 +0200

    Update VC deps

--- nitrogen/generated/android/kotlin/com/margelo/nitro/visioncameraplugininatvision/HybridVisionCameraPluginInatVisionSpec.kt
+++ nitrogen/generated/android/kotlin/com/margelo/nitro/visioncameraplugininatvision/HybridVisionCameraPluginInatVisionSpec.kt
@@ -10,6 +10,7 @@ package com.margelo.nitro.visioncameraplugininatvision
 import androidx.annotation.Keep
 import com.facebook.jni.HybridData
 import com.facebook.proguard.annotations.DoNotStrip
+import dalvik.annotation.optimization.FastNative
 import com.margelo.nitro.core.AnyMap
 import com.margelo.nitro.camera.HybridFrameSpec
 import com.margelo.nitro.core.HybridObject
@@ -44,6 +45,7 @@ abstract class HybridVisionCameraPluginInatVisionSpec: HybridObject() {
   @Keep
   protected open class CxxPart(javaPart: HybridVisionCameraPluginInatVisionSpec): HybridObject.CxxPart(javaPart) {
     // C++ JHybridVisionCameraPluginInatVisionSpec::CxxPart::initHybrid(...)
+    @FastNative
     external override fun initHybrid(): HybridData
   }
   override fun createCxxPart(): CxxPart {
